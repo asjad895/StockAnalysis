e96a1b1e9e4f507b6e91fc2cc4db4640729f4c0a
Md Asjad <mdasjad895@gmail.com>
2023-09-30 21:54:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,8 @@ def plot_hourly_sentiment(parsed_and_scored_news, ticker):
     # mean_scores = parsed_and_scored_news.pivot_table(index=parsed_and_scored_news.index.hour, columns=parsed_and_scored_news.index.date, values=['headline', 'neg', 'neu', 'pos', 'sentiment_score'], aggfunc='first')
     print(mean_scores)
     # mean_scores = mean_scores.dt.to_pydatetime()
-    fig1 = px.bar(mean_scores, x=mean_scores.index, y='sentiment_score', title=f'{ticker} Hourly Sentiment Scores')
+    fig1 = px.bar(mean_scores, x=mean_scores.index, y='sentiment_score', title=f'{ticker} Hourly Sentiment Scores',width=1000
+                  ,height=600)
     # Create a column for color based on sentiment_score
     print("aaa")
     mean_scores['color'] = mean_scores['sentiment_score'].apply(lambda score: 'green' if score > 0 else 'red')
@@ -238,7 +239,8 @@ def plot_hourly_sentiment(parsed_and_scored_news, ticker):
     print("hour2")
     datetime_index = np.array(mean_scores.index)
     fig2 = px.line(mean_scores, x=datetime_index, y='sentiment_score', markers=True,color='color',
-                   title=f'{ticker} Hourly Sentiment Scores (Green: Positive, Red: Negative)')
+                   title=f'{ticker} Hourly Sentiment Scores (Green: Positive, Red: Negative)',width=1000
+                  ,height=600)
     return fig1, fig2
 
 # Define a function for plotting daily sentiment
@@ -247,14 +249,15 @@ def plot_daily_sentiment(parsed_and_scored_news, ticker):
     df = parsed_and_scored_news[['sentiment_score']].copy()
     print(df.head())
     mean_scores_d = df.resample('H').mean()
-    fig1 = px.bar(mean_scores_d, x=mean_scores_d.index, y='sentiment_score', title=f'{ticker} Daily Sentiment Scores',width=800
+    fig1 = px.bar(mean_scores_d, x=mean_scores_d.index, y='sentiment_score', title=f'{ticker} Daily Sentiment Scores',width=1000
                   ,height=600)
     # Create a column for color based on sentiment_score
     mean_scores_d['color'] = mean_scores_d['sentiment_score'].apply(lambda score: 'green' if score > 0 else 'red')
     mean_scores_d.dropna(subset=['sentiment_score'], inplace=True)
     # Create the line plot
     fig2 = px.line(mean_scores_d, x=mean_scores_d.index, y='sentiment_score', color='color',
-                   title=f'{ticker} Daily Sentiment Scores (Green: Positive, Red: Negative)')
+                   title=f'{ticker} Daily Sentiment Scores (Green: Positive, Red: Negative)',width=1000
+                  ,height=600)
     print("daily2")
     return fig1, fig2
 import pandas as pd
